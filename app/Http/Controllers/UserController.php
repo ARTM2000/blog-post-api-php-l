35cbd52346d47237248975be5f801521fdafca45
alireza tanoomandian <tanoomandian204@gmail.com>
2020-09-01 11:50:26 +0430

    creation user functionality added

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    
+    // Creating User by name and email and password
+    public function createUser(Request $req) {
+        $this->validate($req, [
+            "name" => "required",
+            "email" => ["email", "required"],
+            "password" => ["min:8", "required"]
+        ]);
+        
+        $newUser = new User;
+        $newUser->name = $req->name;
+        $newUser->email = $req->email;
+        $newUser->password = $req->password;
+            
+        $result = $newUser->save();
+        if($result) {
+            return response()->json([
+                    "message" => "User created",
+                    "status" => 200,
+                    "user" => $newUser
+                ]);
+        } else {
+            return response()->json([
+                    "message" => "User creation failed",
+                    "status" => 500
+            ]);
+        }
+    }
+
+
+}
